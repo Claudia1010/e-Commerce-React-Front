f704721feeffb9b8a1ebb6d772618108568b5bf6
Claudia1010 <claudiachristensen1@gmail.com>
2022-09-05 20:28:44 +0200

    feat: added Header with logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      {['md'].map((expand) => (
+        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+          <Container fluid>
+          <Navbar.Brand href="/">
+            <img
+              src="/img/Logo.png"
+              height="100"
+              className="d-inline-block align-top"
+              alt="Artech"
+            />
+          </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/paintings">Paintings</Nav.Link>
+                  <NavDropdown
+                    title="Dropdown"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                    <NavDropdown.Item href="/register">
+                      Register
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <span><box-icon name="cart" style={{fontSize: '30px'}} /></span>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
   )
 }
 
