ec2bbd7c9ac3f9dcf46b64f96c949baae90152f3
Claudia1010 <claudiachristensen1@gmail.com>
2022-09-11 14:28:27 +0200

    feat: added user and register views

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'boxicons';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './containers/Home/Home';
-import ProductList from './containers/Product/ProductList';
+
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Header/>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
-          <Route path='/paintings' element={<ProductList/>}></Route>
+  
       </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/AuthUser/AuthUser.js
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function AuthUser(){
+    const navigate = useNavigate();
+
+    const getToken = () =>{
+        const tokenString = sessionStorage.getItem('token');
+    //    console.log(tokenString)
+       if(typeof tokenString === 'undefined'){
+          return false
+        }
+        const userToken = JSON.parse(tokenString);
+        return userToken;
+    }
+
+    const getUser = () =>{
+        const userString = sessionStorage.getItem('user');
+        const user_detail = JSON.parse(userString);
+        return user_detail;
+    }
+
+    const getRole = () =>{
+      const roleString = sessionStorage.getItem('role');
+        const role_user = JSON.parse(roleString);
+
+        if(role_user !== null ){
+        //   console.log(role_user[0].name)
+          return role_user[0].name;
+        }
+
+        return role_user;
+    }
+
+    const [token,setToken] = useState(getToken());
+    const [user,setUser] = useState(getUser());
+    const [role, setRole] = useState(getRole());
+
+    const saveToken = (user,token,role) =>{
+        sessionStorage.setItem('token',JSON.stringify(token));
+        sessionStorage.setItem('user',JSON.stringify(user));
+        sessionStorage.setItem('role',JSON.stringify(role));
+
+        // console.log("esto es nuestro user", user)
+        // console.log("esto es nuestro token", token)
+        // console.log("esto es nuestro token", role)
+
+        setToken(token);
+        setUser(user);
+        setRole(role);
+        navigate('/');
+    }
+
+    const logout = () => {
+        sessionStorage.clear();
+        localStorage.clear();
+        navigate('/');
+    }
+
+    const http = axios.create({
+        baseURL:"http://localhost:8000/api",
+        headers:{
+            "Content-type" : "application/json",
+            "Authorization" : `Bearer ${token}`
+        }
+    });
+    return {
+        setToken:saveToken,
+        token,
+        user,
+        getToken,
+        getRole,
+        getUser,
+        http,
+        logout
+    }
+}
