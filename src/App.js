41b485c56e0a6c85bc00e279b5de17c97f47eeee
Claudia1010 <claudiachristensen1@gmail.com>
2022-09-11 14:36:26 +0200

    Fixed login and register

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import 'boxicons';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './containers/Home/Home';
+import Login from './containers/User/Login/Login';
+import Register from './containers/User/Register/Register';
 
 
 
@@ -10,11 +12,13 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header/>
+      <Header/>    
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
-  
-      </Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/paintings" element={<ProductList />} /> */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
