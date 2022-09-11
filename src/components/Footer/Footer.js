43823dac92a7377cca06e68fd9c3d1930924ccab
Claudia1010 <claudiachristensen1@gmail.com>
2022-09-12 01:03:57 +0200

    added website screenshots

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.scss'
 
 const Footer = () => {
   return (
-    <footer class="w-100 bg-light d-flex align-items-center justify-content-center flex-wrap">
-        <p class="px-3">
+    <footer className="w-100 bg-light d-flex align-items-center justify-content-center flex-wrap">
+        <p className="px-3">
             Replicas Artech &copy; Todos Los Derechos Reservados 2022</p>
-        <p class="px-3"> Piedrabuena 36, 46017 València, Valencia, España</p>
+        <p className="px-3"> Piedrabuena 36, 46017 València, Valencia, España</p>
         <div id="iconos">
             <box-icon type='logo' name='facebook-circle'></box-icon>
             <box-icon name='twitter' type='logo' ></box-icon>
